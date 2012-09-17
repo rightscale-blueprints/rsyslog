2c9402e978ce4cea0aa28823f69172774542f9eb
Chris Fordham <chris.fordham@rightscale.com>
2012-09-17 14:35:10 +1000

    Add removal of .gitignore from collectd cookbook cache.

--- update-metadata.sh
+++ update-metadata.sh
@@ -2,7 +2,7 @@
 
 # generate metadata.json for upstream opscode cookbooks that have no metadata.json
 knife cookbook metadata apache2 -o cookbooks/
-knife cookbook metadata collectd -o cookbooks/
+knife cookbook metadata collectd -o cookbooks/ && rm cookbooks/collectd/.gitignore
 knife cookbook metadata collectd_plugins -o cookbooks/
 knife cookbook metadata cron -o cookbooks/
 knife cookbook metadata ntp -o cookbooks/
